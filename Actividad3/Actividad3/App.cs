e434d1177fed463a13c976b8e119af9f0c72623d
abrahan0108 <cortes.abrahan@yahoo.com.mx>
2016-03-02 18:29:46 -0600

    * App.cs: Puse una etiqueta y un botón
    * repositories.config:

--- Actividad3/Actividad3/App.cs
+++ Actividad3/Actividad3/App.cs
@@ -3,17 +3,42 @@ using Xamarin.Forms;
 
 namespace Actividad3
 {
-	public class App
+	public class ButtonLoggerPage : ContentPage
 	{
-		public static Page GetMainPage ()
-		{	
-			return new ContentPage { 
-				Content = new Label {
-					Text = "Hello, Forms!",
-					VerticalOptions = LayoutOptions.CenterAndExpand,
-					HorizontalOptions = LayoutOptions.CenterAndExpand,
-				},
+		StackLayout loggerLayout = new StackLayout();
+
+		public ButtonLoggerPage()
+		{
+			// Create the Button and attach Clicked handler.
+			Button button = new Button
+			{
+				Text = "Haga click aquí" 
 			};
+			button.Clicked += OnButtonClicked;
+
+			this.Padding = new Thickness(5, Device.OnPlatform(20, 0, 0), 5, 0);
+
+			this.Content = new StackLayout
+			{
+				Children =
+				{
+					button,
+					new ScrollView
+					{
+						VerticalOptions = LayoutOptions.FillAndExpand,
+						Content = loggerLayout
+					}
+				}
+				};
+		}
+
+		void OnButtonClicked(object sender, EventArgs args)
+		{
+
+			loggerLayout.Children.Add(new Label
+				{
+					Text = "Abrahán Cortés Hernández " + DateTime.Now.ToString("T")
+				});
 		}
 	}
 }
