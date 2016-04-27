62ea397884798d105511daf6dc2ca3fb465b41cd
abrahan0108 <cortes.abrahan@yahoo.com.mx>
2016-04-27 11:54:04 -0500

    * Actividad8.projitems: Se han agregado imagenes remotas en el   carrusel
    * App.cs:
    * Login.cs:
    * Contenido.cs:
    * repositories.config:
    * Resource.designer.cs:

--- Actividad8/Actividad8/App.cs
+++ Actividad8/Actividad8/App.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Xamarin.Forms;
+using System.Collections.Generic;
 
 namespace Actividad8
 {
@@ -7,7 +8,53 @@ namespace Actividad8
 	{
 		public static Page GetMainPage ()
 		{	
-			return new NavigationPage (new Login());
+			
+			return new CarouselPage {
+
+				Children ={
+					new ContentPage{
+						Content = new Image
+						{
+							Source = "http://cursosformarte.com/wp-content/uploads/2016/04/Cursos-online-portada-1.jpg"
+
+						}
+					},
+
+					new ContentPage{
+						Content = new Image
+						{
+							Source = "http://bateriaonline.com/wordpress/wp-content/uploads/2013/01/bater%C3%ADa-estandard.png"
+
+						}
+					},
+
+					new ContentPage{
+						Content = new Image
+						{
+							Source = "http://devstickers.com/assets/img/pro/2p4i.png"
+
+						}
+					},
+
+					new ContentPage{
+						Content = new Image
+						{
+							Source = "http://www.iztacala.unam.mx/rrivas/imagenes/Escudos%20UNAM/logounam.jpg"
+
+						}
+					},
+
+					new ContentPage{
+						Content = new Image
+						{
+							Source = "http://www.sefca.com.mx/images/patrocinadores/FCA.png"
+
+						}
+					},
+				}
+
+			};
+
 		}
 	}
 }
